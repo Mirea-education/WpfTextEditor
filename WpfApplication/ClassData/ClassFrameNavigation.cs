c5e346026826a509c75ca53736689c4e328e96fb
Stas Makievsky <makievskij@mirea.ru>
2024-10-07 13:55:15 +0300

    first commit

---
+++ WpfApplication/ClassData/ClassFrameNavigation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApplication.ClassData
+{
+    internal class ClassFrameNavigation
+    {
+        public static Frame frmBodyNav { get; set; }
+    }
+}
